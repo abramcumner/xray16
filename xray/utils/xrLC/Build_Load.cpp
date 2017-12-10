37ac314fb4ed18ed436d6eb82c4a09834a080e30
abramcumner <abramcumner@yandex.ru>
2017-12-10 12:31:07 +0300

    исправлена загрузка текстур в xrLC под х64.

--- xray/utils/xrLC/Build_Load.cpp
+++ xray/utils/xrLC/Build_Load.cpp
@@ -286,16 +286,30 @@ void CBuild::Load	(const b_params& Params, const IReader& _in_FS)
 	{
 		Surface_Init		();
 		F = fs.open_chunk	(EB_Textures);
-		u32 tex_count	= F->length()/sizeof(b_texture);
+#ifdef _WIN64
+		u32 tex_count = F->length() / sizeof(help_b_texture);
+#else
+		u32 tex_count = F->length() / sizeof(b_texture);
+#endif
+		bool is_thm_missing = false;
+		bool is_tga_missing = false;
 		for (u32 t=0; t<tex_count; t++)
 		{
 			Progress		(float(t)/float(tex_count));
 
-			b_texture		TEX;
-			F->r			(&TEX,sizeof(TEX));
-
+#ifdef _WIN64
+			// workaround for ptr size mismatching
+			help_b_texture	TEX;
+			F->r(&TEX, sizeof(TEX));
 			b_BuildTexture	BT;
-			CopyMemory		(&BT,&TEX,sizeof(TEX));
+			CopyMemory(&BT, &TEX, sizeof(TEX) - 4);	// ptr should be copied separately
+			BT.pSurface = (u32*)TEX.pSurface;
+#else
+			b_texture		TEX;
+			F->r(&TEX, sizeof(TEX));
+			b_BuildTexture	BT;
+			CopyMemory(&BT, &TEX, sizeof(TEX));
+#endif
 
 			// load thumbnail
 			LPSTR N			= BT.name;
@@ -314,7 +328,12 @@ void CBuild::Load	(const b_params& Params, const IReader& _in_FS)
 				FS.update_path	(th_name,"$game_textures$",strconcat(sizeof(th_name),th_name,N,".thm"));
 				clMsg			("processing: %s",th_name);
 				IReader* THM	= FS.r_open(th_name);
-				R_ASSERT2		(THM,th_name);
+				if (!THM)
+				{
+					clMsg("cannot find thm: %s", th_name);
+					is_thm_missing = true;
+					continue;
+				}
 
 				// version
 				u32 version = 0;
@@ -346,7 +365,12 @@ void CBuild::Load	(const b_params& Params, const IReader& _in_FS)
 						u32			w=0, h=0;
 						BT.pSurface		=	Surface_Load(N,w,h);
 						BT.THM.SetHasSurface(TRUE);
-						R_ASSERT2	(BT.pSurface,"Can't load surface");
+						if (!BT.pSurface)
+						{
+							clMsg("cannot find tga texture: %s", N);
+							is_tga_missing = true;
+							continue;
+						}
 						if ((w != BT.dwWidth) || (h != BT.dwHeight))
 						{
 							Msg		("! THM doesn't correspond to the texture: %dx%d -> %dx%d", BT.dwWidth, BT.dwHeight, w, h);
@@ -363,6 +387,8 @@ void CBuild::Load	(const b_params& Params, const IReader& _in_FS)
 			// save all the stuff we've created
 			textures().push_back	(BT);
 		}
+		R_ASSERT2(!is_thm_missing, "Some of required thm's are missing. See log for details.");
+		R_ASSERT2(!is_tga_missing, "Some of required tga_textures are missing. See log for details.");
 	}
 
 	// post-process materials
